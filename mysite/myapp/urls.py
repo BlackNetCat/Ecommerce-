db875574351f6327b9b2eb694ee3a69f0b894f42
Melnyk Oleksandr <melnyk.alexandr@gmail.com>
2025-07-23 04:07:46 +0300

    Remove hardcode in links, add namespaces

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "myapp"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("hello/<int:my_id>/", views.indexItem, name="indexItem"),
+    path("hello/<int:my_id>/", views.indexItem, name="detail"),
     path('contacts/', views.contacts, name='contacts')
 
 ]
